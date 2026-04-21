d07d4bd533cbb5e0946e7d8150f5c566f8f3b74d
MiqueasReyes <miqueasreyescruz@gmail.com>
2026-04-20 22:31:15 -0300

    Agrego interfaz de usuario a ArregloDeNombres.c

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -9,6 +9,7 @@ void BuscarNombrePorId (char *V[], int id);
 
 int main () {
     char *V[5], buffer[50];
+    int opcion, id;
 
     for (int i = 0; i < MAX; i++)
     {
@@ -20,6 +21,36 @@ int main () {
     }
 
     MostrarPersonas(V);
+
+    do
+    {
+        printf("__________MENU__________\n");
+        printf("1. Buscar por clave\n");
+        printf("2. Buscar por id\n");
+        printf("3. Salir\n");
+        printf("-Ingrese una opcion: ");
+
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+            case 1:
+                printf("Ingrese la clave a buscar: ");
+                scanf("%s", buffer);
+                puts(BuscarNombrePorClave(V, buffer));
+                break;
+            case 2:
+                printf("Ingrese el id a buscar:");
+                scanf("%d", &id);
+                BuscarNombrePorId(V, id);
+                break;
+            default:
+                printf("Finalizando...");
+                break;
+        }
+
+    } while (opcion != 3 && opcion > 0 && opcion < 3);
+    
 
     for (int i = 0; i < MAX; i++)
     {
